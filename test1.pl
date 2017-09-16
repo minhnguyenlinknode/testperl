5a34517a72a223f337da1b4816ecc36a7937ae0e
Minh Ng <quy.minh@yahoo.com>
2017-09-17 00:06:29 +0100

    Update test1.pl

--- test1.pl
+++ test1.pl
@@ -1,26 +1,32 @@
 #!/usr/bin/perl
 
+# Initial array pointer
 my  $refStructure = [ 
     { a => 1, b => 2, c => 3 }, 
     { d => 4, e => 5 },
     [ 6, 7, 8 ], 9, 10, 11, [ 12, 13, 14 ] 
 ];
 
+# Recursive function
 sub addtwo {
     my $s = shift;
 
-    if (ref($s) eq "ARRAY") {
+    #if is array pointer
+    if (ref($s) eq "ARRAY") { 
         for my $e (@$s) {
-            ref $e ? addtwo( $e ) : $e++;
+	    #check to repeat for sub array pointer
+            ref $e ? addtwo( $e ) : $e++; 
         } 
     }
-    elsif (ref($s) eq "HASH") {
+    #if is hash pointer
+    elsif (ref($s) eq "HASH") { 
         for my $key (keys %$s) {
             $s->{$key}++;
         }
     }
 }
 
+# This is just for output testing
 sub printResult {    
   for my $e (@$refStructure) {       
       if (ref($e) eq "ARRAY") {
